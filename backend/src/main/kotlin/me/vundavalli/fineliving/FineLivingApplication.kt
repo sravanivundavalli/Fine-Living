90a1cadc9c9ef8b8728d5e5e48337ca0f082d5a4
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2023-08-09 14:43:14 +0200

    initial commit

---
+++ backend/src/main/kotlin/me/vundavalli/fineliving/FineLivingApplication.kt
@@ -0,0 +1,11 @@
+package me.vundavalli.fineliving
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FineLivingApplication
+
+fun main(args: Array<String>) {
+	runApplication<FineLivingApplication>(*args)
+}
---
+++ backend/src/test/kotlin/me/vundavalli/fineliving/FineLivingApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.vundavalli.fineliving
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FineLivingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
